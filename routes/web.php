5c03fce5398a91816804b789e97023c73cef5827
zohaib mukhtar <mukhtarzohaib38@gmail.com>
2025-06-12 19:04:26 +0500

    code extended

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Http;  // <-- Add this
+use Illuminate\Support\Facades\Http; 
 
 Route::get('/', function () {
     return view('welcome');
